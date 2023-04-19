459c34c742ac5278cb3815f964d92ae20c2c0ea0
Raul Werner <qrananico25@gmail.com>
2023-04-19 14:59:09 -0300

    etapas 1 e 2 concluidas

--- principal/main.py
+++ principal/main.py
@@ -2,16 +2,23 @@ from funcoes import limpar_tela
 
 limpar_tela()
 
+print("-="*20)
+print("JOGO DA FORCA")
+print("-="*20)
 nome_desafiante = input("Nome do desafiante: ")
 nome_competidor = input("Nome do competidor: ")
 
 limpar_tela()
 
 palavra_chave = input("Digite a palavra chave: ")
+dica1 = input("Digite a dica 1: ")
+dica2 = input("Digite a dica 2: ")
+dica3 = input("Digite a dica 3: ")
 
 limpar_tela()
 
-print("-="*20)
-print("JOGO DA FORCA")
-print("Seja bem vindo!")
-print("-="*20)
+print("A palavra chave contém", len(palavra_chave), "letras!")
+input("clique 'enter' para continuar...")
+limpar_tela()
+
+jogo_dica = input("")
