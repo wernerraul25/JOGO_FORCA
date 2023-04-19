1af7b9d9b02af7a404855fd80948315dcf397b16
Raul Werner <qrananico25@gmail.com>
2023-04-19 15:39:31 -0300

    avançando na interface

--- principal/main.py
+++ principal/main.py
@@ -5,13 +5,18 @@ limpar_tela()
 print("-="*20)
 print("JOGO DA FORCA")
 print("-="*20)
+input("Press enter to continue...")
+
+limpar_tela()
+
 nome_desafiante = input("Nome do desafiante: ")
 nome_competidor = input("Nome do competidor: ")
 
 limpar_tela()
 
 palavra_chave = input("Digite a palavra chave: ")
-dicas = [input("Dica 1: "),input("Dica 2: "),input("Dica 3: ")]
+dicas = [input("Dica 1: "), input("Dica 2: "), input("Dica 3: ")]
+
 
 limpar_tela()
 
@@ -20,7 +25,14 @@ input("press enter to continue...")
 limpar_tela()
 
 print("Selecione a opção que deseja digitando o número equivalente!")
-jogo_dica = input("(0)Jogar \n(1)Solicitar dica\n")
+jogo_dica = input(str("(0)Jogar \n(1)Solicitar dica\n"))
+
+if jogo_dica == "0":
+    input(str("Informe uma letra: "))
+elif jogo_dica =="1":
+    for dica in dicas:
+        print(dica)
+
 
 dicas_solicitadas = 0
 chances = 5
