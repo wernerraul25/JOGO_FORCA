b71ee00eaa1fcbc4841217cd359a225e829cf46d
Raul Werner <qrananico25@gmail.com>
2023-04-21 15:30:46 -0300

    def criar base de dados

--- principal/funcoes.py
+++ principal/funcoes.py
@@ -8,3 +8,13 @@ def tela_inicial():
     print("JOGO DA FORCA")
     print("*-"*10)
     input("Press enter to continue...")
+
+def criar_base_dados():
+    try:
+        arquivo = open("banco_dados.txt","r")
+        arquivo.close()
+    except:
+        arquivo = open("banco_dados.txt","w")
+        print("Base de dados criada com sucesso")
+        input("Press enter to continue...")
+        arquivo.close()
