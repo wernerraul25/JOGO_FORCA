c2eea194981f383e23794ad4dfe2b45494d654c1
Raul Werner <qrananico25@gmail.com>
2023-04-21 16:04:20 -0300

    ainda com bugs, mas funcionando melhor

--- principal/main.py
+++ principal/main.py
@@ -12,49 +12,52 @@ nome_competidor = input("Nome do competidor: ")
 limpar_tela()
 
 palavra_chave = input("Digite a palavra chave: ")
-letras_descobertas = []
-dicas = [input("Dica 1: "), input("Dica 2: "), input("Dica 3: ")]
+dica1 = input("Dica 1: ")
+dica2 = input("Dica 2: ")
+dica3 = input("Dica 3: ")
 
+letras_descobertas = []
 
 limpar_tela()
 
-print("A palavra chave contém", len(palavra_chave), "letra(s)!")
+tamanho_palavra_chave = len(palavra_chave)
+palavra_oculta = '*' * tamanho_palavra_chave
+dicas_solicitadas = 0
+
+print("A palavra chave contém", tamanho_palavra_chave, "letra(s)!")
 input("press enter to continue...")
+
 limpar_tela()
 
 print("Selecione a opção que deseja digitando o número equivalente!")
-opcoes_de_jogo = input(str("(0)Jogar \n(1)Solicitar dica\n(2)Sair do jogo\n"))
+opcao = input("(0)Solicitar dica \n(1)Jogar\n(2)Sair do jogo\n")
 
-chances = 5
-dicas_solicitadas = 0
-
+aleatorio = 0
 try:
-    while chances > 0:
-        if opcoes_de_jogo == "0":
-            for i in range(0, len(palavra_chave)):
-                letras_descobertas.append("*")
-
-            acertou = False
-
-            while acertou == False :
-                letra = str(input("Digite a letra: "))
-
-                for i in range(0, len(palavra_chave)):
-                    if letra == palavra_chave[i]:
-                        letras_descobertas[i] = letra
-
-                    print(letras_descobertas[i])
-
-                acertou = True
-
-                for x in range(0, len(letras_descobertas)):
-                    if letras_descobertas[x] == "*":
-                        acertou = False
+    while aleatorio < 2:
+        aleatorio += 1
+        if opcao =="0":
+            dicas_solicitadas += 1
+            if dicas_solicitadas > 3:
+                print("Você solicitou o número máximo de dicas!")
+            else:
+                if dicas_solicitadas == 1:
+                    print("Dica 1:", dica1)
+                    continue
+                elif dicas_solicitadas == 2:
+                    print("Dica 2:", dica2)
+                    continue
+                else:
+                    print("Dica 3:", dica3)
+                #bug, fecha o jogo ao pedir dica
+        elif opcao == "1":
+            letra = input("Digite uma letra: ")
+            
         
-        elif opcoes_de_jogo =="1":#resolver como mostrar uma dica por vez
-            print(dicas)
-            dicas_solicitadas = dicas_solicitadas + 1
-        elif opcoes_de_jogo =="2":
-            break
+        #elif opcao =="2":
+        #falta opção 2
+        else:
+            print("Opção inválida!")
+            continue   
 except:
     print("O jogo acabou!")
